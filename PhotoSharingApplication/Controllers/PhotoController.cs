cf793417a380fd78ec8f39a27aafe0fa9d65a29e
Martina <m.lusardi@hotmail.com>
2021-11-08 18:31:38 -0300

    Integrador Completo

--- PhotoSharingApplication/Controllers/PhotoController.cs
+++ PhotoSharingApplication/Controllers/PhotoController.cs
@@ -22,9 +22,34 @@ namespace PhotoSharingApplication.Controllers
         // GET: Photo
         public ActionResult Index()
         {
-            return View("Index", context.Photos.ToList());
+            return View("Index");
 
         }
+
+
+
+
+        [ChildActionOnly]
+        public ActionResult _PhotoGallery(int number = 0)
+        {
+            List<Photo> photos;
+            if (number == 0)
+            {
+                photos = context.Photos.ToList();
+            }
+            else
+            {
+                photos = (
+                from p in context.Photos
+                orderby p.CreatedDate descending
+                select p).Take(number).ToList();
+            }
+            return PartialView("_PhotoGallery", photos);
+        }
+
+
+
+
 
 
 
@@ -145,7 +170,6 @@ namespace PhotoSharingApplication.Controllers
             {
                 return null;
             }
-
 
         }
 
