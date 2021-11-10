9334e7c134cee7ff54889efed713bc852ffb5165
Martina <m.lusardi@hotmail.com>
2021-11-10 17:37:02 -0300

    PhotoSharingWithErrorCompleted

--- PhotoSharingApplication/Controllers/PhotoController.cs
+++ PhotoSharingApplication/Controllers/PhotoController.cs
@@ -10,168 +10,169 @@ using PhotoSharingApplication.Models;
 
 
 
+
 namespace PhotoSharingApplication.Controllers
-{   
+{
+    [HandleError(View = "Error")]
+    [ValueReporterController]
     public class PhotoController : Controller
     {
-        private PhotoSharingContext context = new PhotoSharingContext();
+            private PhotoSharingContext context = new PhotoSharingContext();
 
-
-
-
-        // GET: Photo
-        public ActionResult Index()
-        {
-            return View("Index");
-
-        }
-
-
-
-
-        [ChildActionOnly]
-        public ActionResult _PhotoGallery(int number = 0)
-        {
-            List<Photo> photos;
-            if (number == 0)
+            // GET: Photo
+            public ActionResult Index()
             {
-                photos = context.Photos.ToList();
+                return View("Index");
+
             }
-            else
+
+      
+
+            [ChildActionOnly]
+            public ActionResult _PhotoGallery(int number = 0)
             {
-                photos = (
-                from p in context.Photos
-                orderby p.CreatedDate descending
-                select p).Take(number).ToList();
-            }
-            return PartialView("_PhotoGallery", photos);
-        }
-
-
-
-
-
-
-
-
-
-        public ActionResult Display(int id)
-        {
-            Photo photo = context.Photos.Find(id);
-            if (photo == null)
-            {
-                return HttpNotFound();
-            }
-            return View("Display", photo);
-
-        }
-
-
-
-
-
-        public ActionResult Create()
-        {
-            Photo newPhoto = new Photo();
-            newPhoto.CreatedDate = DateTime.Today;
-            return View("Create", newPhoto);
-
-        }
-
-
-
-
-
-
-
-
-        [HttpPost]
-        public ActionResult Create(Photo photo, HttpPostedFileBase image)
-        {
-            photo.CreatedDate = DateTime.Today;
-            if (!ModelState.IsValid)
-            {
-                return View("Create", photo);
-            }
-            else
-            {
-                if (image != null)
+                List<Photo> photos;
+                if (number == 0)
                 {
-                    photo.ImageMimeType =image.ContentType;
-                    photo.PhotoFile = new byte[image.ContentLength];
-                    image.InputStream.Read(photo.PhotoFile, 0,image.ContentLength);
+                    photos = context.Photos.ToList();
+                }
+                else
+                {
+                    photos = (
+                    from p in context.Photos
+                    orderby p.CreatedDate descending
+                    select p).Take(number).ToList();
+                }
+                return PartialView("_PhotoGallery", photos);
+            }
+
+
+
+
+
+
+
+
+
+            public ActionResult Display(int id)
+            {
+                Photo photo = context.Photos.Find(id);
+                if (photo == null)
+                {
+                    return HttpNotFound();
+                }
+                return View("Display", photo);
+
+            }
+
+
+
+
+
+            public ActionResult Create()
+            {
+                Photo newPhoto = new Photo();
+                newPhoto.CreatedDate = DateTime.Today;
+                return View("Create", newPhoto);
+
+            }
+
+
+
+
+
+
+
+
+            [HttpPost]
+            public ActionResult Create(Photo photo, HttpPostedFileBase image)
+            {
+                photo.CreatedDate = DateTime.Today;
+                if (!ModelState.IsValid)
+                {
+                    return View("Create", photo);
+                }
+                else
+                {
+                    if (image != null)
+                    {
+                        photo.ImageMimeType = image.ContentType;
+                        photo.PhotoFile = new byte[image.ContentLength];
+                        image.InputStream.Read(photo.PhotoFile, 0, image.ContentLength);
+                    }
+
+                }
+                context.Photos.Add(photo);
+                context.SaveChanges();
+                return RedirectToAction("Index");
+
+            }
+
+
+
+
+
+
+            public ActionResult Delete(int id)
+            {
+
+                Photo photo = context.Photos.Find(id);
+
+                if (photo == null)
+                {
+                    return HttpNotFound();
+                }
+
+                return View("Delete", photo);
+
+            }
+
+
+
+
+
+
+
+            [HttpPost]
+            [ActionName("Delete")]
+            public ActionResult DeleteConfirmed(int id)
+            {
+
+                Photo photo = context.Photos.Find(id);
+                context.Photos.Remove(photo);
+                context.SaveChanges();
+                return RedirectToAction("Index");
+
+            }
+
+
+
+
+
+
+
+
+            public FileContentResult GetImage(int id)
+            {
+                Photo photo = context.Photos.Find(id);
+
+                if (photo != null)
+                {
+                    return File(photo.PhotoFile,
+                    photo.ImageMimeType);
+                }
+                else
+                {
+                    return null;
                 }
 
             }
-            context.Photos.Add(photo);
-            context.SaveChanges();
-            return RedirectToAction("Index");
-
-        }
 
 
-
-
-
-
-        public ActionResult Delete(int id)
+        public ActionResult SlideShow()
         {
-
-            Photo photo = context.Photos.Find(id);
-
-            if (photo == null)
-            {
-                return HttpNotFound();
-            }
-
-            return View("Delete", photo);
-
+            throw new NotImplementedException("The Slideshow action is not yet ready.");
         }
-
-
-
-
-
-
-
-        [HttpPost]
-        [ActionName("Delete")]
-        public ActionResult DeleteConfirmed(int id)
-        {
-
-            Photo photo = context.Photos.Find(id);
-            context.Photos.Remove(photo);
-            context.SaveChanges();
-            return RedirectToAction("Index");
-
-        }
-
-
-
-
-
-
-
-
-        public FileContentResult GetImage(int id)
-        {
-            Photo photo = context.Photos.Find(id);
-
-            if (photo != null)
-            {
-                return File(photo.PhotoFile,
-                photo.ImageMimeType);
-            }
-            else
-            {
-                return null;
-            }
-
-        }
-
-
-
-
 
 
 
